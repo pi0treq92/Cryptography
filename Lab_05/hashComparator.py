81ef6ff0fdfb43387d96c3c79455e9f2be6a0b6c
pi0treq92 <32812870+pi0treq92@users.noreply.github.com>
2019-12-03 07:46:02 +0100

    poprawki

--- Lab_05/hashComparator.py
+++ Lab_05/hashComparator.py
@@ -27,6 +27,7 @@ def prepareString(l1):
     return lstring
 
 def compare(preparel1, preparel2, index, hashesPersonal, hashesPersonal_):
+    string = ''
     counter = 0
     table = ["md5sum", "sha1sum", "sha224sum", "sha256sum", "sha384sum", "sha512sum"]
     for i in range(len(preparel1)):
@@ -34,11 +35,13 @@ def compare(preparel1, preparel2, index, hashesPersonal, hashesPersonal_):
             counter+=1
         else:
             continue
-    print("cat hash.pdf personal.txt | {}\ncat hash.pdf personal_.txt | {}\n{}\n{}".format(table[index], table[index],
-                                                                                       hashesPersonal[:-4], hashesPersonal_[:-4]))
-    print("Liczba roznych bitow {}/{}, {}% wszystkich bitow\n".format(counter, len(preparel1), int((counter/len(preparel1))*100)))
+    string = "\ncat hash.pdf personal.txt | {}\ncat hash.pdf personal_.txt | {}\n{}\n{}\n".format(table[index], table[index],
+                                                                                       hashesPersonal[:-4], hashesPersonal_[:-4])
+    string+="Liczba roznych bitow {}/{}, {}% wszystkich bitow\n".format(counter, len(preparel1), int((counter/len(preparel1))*100))
+    return string
 
 def compareHashes(hash1, hash2, hashes):
+    string = ''
     if len(hash1) != len(hash2):
         raise ValueError("Length of hashes are not equal")
     else:
@@ -49,12 +52,15 @@ def compareHashes(hash1, hash2, hashes):
             if len(hash1[i]) != len(hash2[i]):
                 raise ValueError("Length of hashes are not equal")
             else:
-                compare(hashString1, hashString2, counter, hashes[counter], hashes[counter+1])
+                string+=compare(hashString1, hashString2, counter, hashes[counter], hashes[counter+1])
             counter += 1
+    return string
 def main():
+    diff = open("diff.txt", 'w')
     personal = prepareBin(hashpersonal)
     personal_ = prepareBin(hashpersonal_)
-    compareHashes(personal, personal_, hashes)
+    diff.write(compareHashes(personal, personal_, hashes))
+    diff.close()
 
 if __name__ == "__main__":
     main()
