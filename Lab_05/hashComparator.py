1d378687787dc43be1dc4350036195abd9e799b2
pi0treq92 <32812870+pi0treq92@users.noreply.github.com>
2019-12-03 07:38:49 +0100

    Final

--- Lab_05/hashComparator.py
+++ Lab_05/hashComparator.py
@@ -1,9 +1,8 @@
 hashFile = open("hash.txt", 'r')
 hashes = hashFile.readlines()
-print(len(hashes))
 def divideHashes(hashes):
     hashpersonal = [x for x in hashes[0:len(hashes)-1:2]]
-    hashpersonal_ = [x for x in hashes[1:len(hashes) - 1:2]]
+    hashpersonal_ = [x for x in hashes[1:len(hashes):2]]
     return hashpersonal, hashpersonal_
 
 
@@ -21,13 +20,42 @@ def prepareBin(hash):
         binTable.append(binar)
     return binTable
 
-def compareHashes(hash1, hash2):
+def prepareString(l1):
+    lstring = ''
+    for i in range(len(l1)):
+            lstring+=l1[i]
+    return lstring
+
+def compare(preparel1, preparel2, index, hashesPersonal, hashesPersonal_):
+    counter = 0
+    table = ["md5sum", "sha1sum", "sha224sum", "sha256sum", "sha384sum", "sha512sum"]
+    for i in range(len(preparel1)):
+        if preparel1[i] != preparel2[i]:
+            counter+=1
+        else:
+            continue
+    print("cat hash.pdf personal.txt | {}\ncat hash.pdf personal_.txt | {}\n{}\n{}".format(table[index], table[index],
+                                                                                       hashesPersonal[:-4], hashesPersonal_[:-4]))
+    print("Liczba roznych bitow {}/{}, {}% wszystkich bitow\n".format(counter, len(preparel1), int((counter/len(preparel1))*100)))
+
+def compareHashes(hash1, hash2, hashes):
     if len(hash1) != len(hash2):
         raise ValueError("Length of hashes are not equal")
     else:
+        counter = 0
         for i in range(len(hash1)):
+            hashString1 = prepareString(hash1[i])
+            hashString2 = prepareString(hash2[i])
+            if len(hash1[i]) != len(hash2[i]):
+                raise ValueError("Length of hashes are not equal")
+            else:
+                compare(hashString1, hashString2, counter, hashes[counter], hashes[counter+1])
+            counter += 1
+def main():
+    personal = prepareBin(hashpersonal)
+    personal_ = prepareBin(hashpersonal_)
+    compareHashes(personal, personal_, hashes)
 
-
-personal = prepareBin(hashpersonal)
-personal_ = prepareBin(hashpersonal_)
+if __name__ == "__main__":
+    main()
 
