28b61423c4baddcd313dcbbc1c03050cf654f8bf
pi0treq92 <32812870+pi0treq92@users.noreply.github.com>
2020-01-11 16:29:35 +0100

    newest_version

--- Lab_07/stegano.py
+++ Lab_07/stegano.py
@@ -28,16 +28,14 @@ def wynurz2():
     with open('watermark.html') as inf:
         txt = inf.read()
     docPlain = bs4.BeautifulSoup(txt, features="html.parser")
-    counter = 0
     paragraphCounter = 0
     crypto = ''
-    body = ''
-    done = 0
     check = False
     for j in range(paragraphCounter, len(docPlain.find_all('p'))):
-                if done == 1:
-                    done = 0
-                    break
+        if crypto.__len__() >= 256:
+            break
+        else:
+                counter = 0
                 if docPlain.find_all('p')[j].string != None:
                         for k in range(counter, len(docPlain.find_all('p')[j].string)):
                             if check == True:
@@ -48,32 +46,27 @@ def wynurz2():
                                 break
                             counter += 1
                             if docPlain.find_all('p')[j].string[k] == ' ':
-                                if docPlain.find_all('p')[j].string[k+1] == ' ':
-                                    crypto += '1'
-                                    check = True
-                                    continue
-                                else:
-                                    crypto += '0'
+                                try:
+                                    if docPlain.find_all('p')[j].string[k+1] == ' ':
+                                        crypto += '1'
+                                        check = True
+                                        continue
+                                    else:
+                                        crypto += '0'
+                                        continue
+                                except:
                                     continue
                             else:
                                 continue
-    bitTable = []
-    bitString = ''
     decrypt = ''
-    for i in range(256):
-        if txt[i][-2] == ' ':
-            bitString += '1'
-        else:
-            bitString += '0'
-        if len(bitString) % 4 == 0:
-            bitTable.append(bitString)
-            bitString = ''
-    for j in range(len(bitTable)):
-        decrypt += hex(int(bitTable[j], 2))[2]
+    if len(crypto) > 256:
+        crypto = crypto[:256]
+    split_list = [crypto[x:x + 4] for x in range(0, len(crypto), 4)]
+    for i in range(len(split_list)):
+        decrypt += hex(int(split_list[i], 2))[2]
     cryptoFile = open('detect.txt', 'w')
     cryptoFile.write(decrypt)
     cryptoFile.close()
-
 
 def zanurz2():
     cryptoFile = open('mess.txt', 'r')
@@ -99,7 +92,6 @@ def zanurz2():
                         paragraphCounter += 1
                         body = ''
                         break
-
                     else:
                         for k in range(counter, len(docPlain.find_all('p')[j].string)):
                             if len(docPlain.find_all('p')[j].string) <= counter:
@@ -143,12 +135,11 @@ def zanurz2():
                             if docPlain.find_all('p')[j].string[k] == ' ':
                                 done = 1
                                 break
-    d = len(docPlain.p.string)
     docPlain = [item for item in str(docPlain).split('\n') if item != '']
-    a = 0
     watermarkFile = open('watermark.html', 'w')
     watermarkFile.writelines(["%s\n" % item for item in docPlain])
     watermarkFile.write(str(docPlain))
+
 def zanurz():
     with open('cover.html') as inf:
         txt = inf.read()
@@ -166,9 +157,15 @@ def zanurz():
 
 def main():
     if sys.argv[1] == '-e':
-        wynurz2()
+        if sys.argv[2] == '-1':
+            zanurz()
+        elif sys.argv[2] == '-2':
+            zanurz2()
     elif sys.argv[1] == '-d':
-        wynurz()
+        if sys.argv[2] == '-1':
+            wynurz()
+        elif sys.argv[2] == '-2':
+            wynurz2()
 
 main()
 cryptoFile = open('mess.txt', 'r')
